8b185595db91051679b11669a316daa729b562d4
walkuf <76922736+walkuf@users.noreply.github.com>
2025-02-11 11:52:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='cbpi4-Flowmeter',
+setup(name='cbpi4-Flowmeter-MCP3008',
       version='0.0.6',
-      description='CraftBeerPi4 Flowsensor / Step Plugin ',
-      author='Alexander Vollkopf',
-      author_email='avollkopf@web.de',
-      url='https://github.com/PiBrewing/cbpi4-Flowmeter',
+      description='CraftBeerPi4 Flowsensor MCP3008 / Step Plugin ',
+      author='Rick Walkup',
+      author_email='walkuf@gmail.com',
+      url='https://github.com/walkuf/cbpi4-Flowmeter-MCP3008',
       include_package_data=True,
       keywords='globalsettings',
       package_data={
         # If any package contains *.txt or *.rst files, include them:
       '': ['*.txt', '*.rst', '*.yaml'],
-      'cbpi4-Flowmeter': ['*','*.txt', '*.rst', '*.yaml']},
-      packages=['cbpi4-Flowmeter'],
+      'cbpi4-Flowmeter-MCP3008': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-Flowmeter-MCP3008'],
       install_requires=[
           'cbpi4>=4.1.10.rc2'
       ],
